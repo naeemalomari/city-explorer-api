f03dd13008bfdc6fa2e38f973e5cf505b8174e8b
naeemalomari <naeemalomari96@gmail.com>
2021-08-03 23:38:04 +0300

    editing the code

--- server.js
+++ server.js
@@ -1,67 +1,66 @@
-'use strict';
+"use strict";
 
-
-const express =require('express');
-const server =express();
-const locData=require('./weather.json');
-const PORT =3001;
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const server = express();
+const locData = require("./weather.json");
+const PORT = 3001;
+server.use(cors());
 
 //My Root Route : http://localhost:3001/
-class serve extends express {
-constructor(props){
-super(props);
-this.state={
-date:'',
-description:'',
-}
 
-}}
-
-server.get('/',(req,res) => {
-    res.send('this is my root route')
-})
+server.get("/", (req, res) => {
+  res.send("this is my root route");
+});
 // http://localhost:3001/test
-server.get('/test',(request,response) => {
-    let str = 'hello from the server side '
-    response.send(str);
-})
+server.get("/test", (request, response) => {
+  let str = "hello from the server side ";
+  response.send(str);
+});
 
-// http://localhost:3001/weather (/weather === route)
-server.get('/Weather',(req,res) => {
-    // console.log(weatherData);
-    let locWeather = locData.map(item => {
-        return item.city_name;
-    });
-    if(locWeather =='Amman' || locWeather =='Seatle' || locWeather =='Paris'){
-    res.status(200).send(locWeather);
-    }else{
-        res.status(404).send('not found');
+// http://localhost:3001/weather?lat=31.95&lon=35.91&searchQuery=Amman
+server.get("/weather", (req, res) => {
+  // console.log(weatherData);
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  const city_name = req.query.searchQuery.toLocaleLowerCase();
+
+
+  let result = locData.find(item => item.lat === lat && item.lon === lon && item.city_name.toLocaleLowerCase() === city_name ? item : "" );
+    result  ? res.send(forCastObject(result)) : res.status(500).send("PAGE NOT FOUND FIND YOUR ERROR");
+    console.log(result);
+  
+
+});
+
+const forCastObject = (weatherObj) => {
+  const forCastList = [];
+  weatherObj.data.map( item => {
+    const description = `low of ${item.low_temp} , high of ${item.high_temp} and ${item.weather.description}`;
+    const date = item.datetime;
+    forCastList.push(new ForCast(date, description));
+
+  });
+  return forCastList;
+};
+ 
+
+class ForCast{
+    constructor(date = '', description = '') {
+      this.date=date;
+      this.description = description;
     }
+  }
+
+
+server.listen(PORT, () => {
+  console.log(`I am listening to  PORT = ${PORT}`);
+  // console.log(require);
 });
 
-//http://localhost:3001/lat (/lat ===route)
-server.get('/lat',(req,res) => {
-    let latitude=locData.find(item => {
-        return item.lat;
-    });
-    res.send(latitude);
-}) ;
 
-
-//http://localhost:3001/lon (/lon === route )
-server.get('/lon',(req,res) => {
-let longitudinal=locData.find(item => {
-return item.lon;
-});
-res.send(longitudinal);
-});
-//////////////////////////////////////////////////////////
-server.get('*',(req,res)=> {
-    res.status(404).send('not found page at all please check the route')
-} )
-
-server.listen(PORT,()=> { 
-    console.log(`I am listening to  PORT = ${PORT}`);
-    // console.log(require);
-});
-
+server.get("*", (req, res) => {
+    res.status(404).send("not found page at all please check the route");
+  });
+  
